ac87842edd406924cbac9120938dea66046d0053
Dusty Leary <dleary@gmail.com>
2012-04-16 18:19:00 -0700

    fix several bugs, add tests

--- index.js
+++ index.js
@@ -5,19 +5,42 @@ var LRU = exports.LRU = function (max) {
     this.max = max || 1000;
 };
 
+LRU.prototype.remove = function (key) {
+  var element = this.cache[key];
+  if(element) {
+    delete this.cache[key];
+    --this.length;
+    if(element.prev) this.cache[element.prev].next = element.next;
+    if(element.next) this.cache[element.next].prev = element.prev;
+    if(this.head == key) {
+      this.head = element.prev;
+    }
+    if(this.tail == key) {
+      this.tail = element.next;
+    }
+  }
+  return element;
+}
+
 LRU.prototype.set = function (key, value) {
-    this.cache[key] = {
-        next: null
-      , prev: this.head
-      , value: value
-    };
+    element = this.remove(key);
+    element = element || { value:value };
+
+    element.next = null;
+    element.prev = this.head;
+
+    this.cache[key] = element;
+
     if (this.head) {
         this.cache[this.head].next = key;
+        element.prev = this.head;
     }
     this.head = key;
-    if (!this.tail) {
-        this.tail = key;
+
+    if(!this.tail) {
+      this.tail = key;
     }
+
     if (++this.length > this.max) {
         this.evict();
     }
@@ -25,31 +48,14 @@ LRU.prototype.set = function (key, value) {
 
 LRU.prototype.get = function (key) {
     var element = this.cache[key];
-    if (!element) {
-        return;
-    }
-    if (element.next) {
-        this.cache[element.next].prev = element.prev;
-    }
-    if (element.prev) {
-        this.cache[element.prev].next = element.next;
-    } else {
-        this.tail = element.next || key;
-    }
-    element.prev = this.head;
-    element.next = null;
-    if (this.head) {
-        this.cache[this.head].next = key;
-    }
-    this.head = key;
+    if (!element) { return; }
+
+    this.set(key, element.value);
     return element.value;
 };
 
 LRU.prototype.evict = function () {
-    var tail = this.cache[this.tail].next;
-    delete this.cache[this.tail];
-    this.cache[tail].prev = null;
-    this.tail = tail;
-    this.length--;
+    if(!this.tail) { return; }
+    this.remove(this.tail);
 };
 
---
+++ test/lru-test.js
@@ -0,0 +1,127 @@
+var assert = require('assert');
+var vows = require('vows');
+var LRU = require('../index');
+
+function keys(obj) {
+  var result = [];
+  for(k in obj) {
+    if(obj.hasOwnProperty(k)) {
+      result.push(k);
+    }
+  }
+  return result;
+}
+
+var suite = vows.describe('LRU');
+
+suite.addBatch({
+  "setting keys doesn't grow past max size": function() {
+    var lru = new LRU.LRU(3);
+    assert.equal(0, lru.length);
+    lru.set('foo1', 'bar1');
+    assert.equal(1, lru.length);
+    lru.set('foo2', 'bar2');
+    assert.equal(2, lru.length);
+    lru.set('foo3', 'bar3');
+    assert.equal(3, lru.length);
+
+    lru.set('foo4', 'bar4');
+    assert.equal(3, lru.length);
+  }
+});
+
+suite.addBatch({
+  "lru invariant is maintained for set()": function() {
+    var lru = new LRU.LRU(2);
+
+    lru.set('foo1', 'bar1');
+    lru.set('foo2', 'bar2');
+    lru.set('foo3', 'bar3');
+    lru.set('foo4', 'bar4');
+
+    assert.deepEqual(['foo3','foo4'], keys(lru.cache));
+  }
+})
+
+suite.addBatch({
+  "lru invariant is maintained for get()": function() {
+    var lru = new LRU.LRU(2);
+
+    lru.set('foo1', 'bar1');
+    lru.set('foo2', 'bar2');
+
+    lru.get('foo1'); //now foo2 should be deleted instead of foo1
+
+    lru.set('foo3', 'bar3');
+
+    assert.deepEqual(['foo1','foo3'], keys(lru.cache));
+  }
+});
+
+suite.addBatch({
+  "idempotent 'changes'": {
+    "set() and remove() on empty LRU is idempotent": function() {
+      var lru = new LRU.LRU();
+      var json1 = JSON.stringify(lru);
+
+      lru.set('foo1', 'bar1');
+      lru.remove('foo1');
+      var json2 = JSON.stringify(lru);
+
+      assert.deepEqual(json2, json1);
+    },
+
+
+    "2 set()s and 2 remove()s on empty LRU is idempotent": function() {
+      var lru = new LRU.LRU();
+      var json1 = JSON.stringify(lru);
+
+      lru.set('foo1', 'bar1');
+      lru.set('foo2', 'bar2');
+      lru.remove('foo1');
+      lru.remove('foo2');
+      var json2 = JSON.stringify(lru);
+
+      assert.deepEqual(json2, json1);
+    },
+
+    "2 set()s and 2 remove()s (in opposite order) on empty LRU is idempotent": function() {
+      var lru = new LRU.LRU();
+      var json1 = JSON.stringify(lru);
+
+      lru.set('foo1', 'bar1');
+      lru.set('foo2', 'bar2');
+      lru.remove('foo2');
+      lru.remove('foo1');
+      var json2 = JSON.stringify(lru);
+
+      assert.deepEqual(json2, json1);
+    },
+
+    "after setting one key, get() is idempotent" : function() {
+      var lru = new LRU.LRU(2);
+      lru.set('a', 'a');
+      var json1 = JSON.stringify(lru);
+
+      lru.get('a');
+      var json2 = JSON.stringify(lru);
+
+      assert.equal(json2, json1);
+    },
+
+    "after setting two keys, get() on last-set key is idempotent" : function() {
+      var lru = new LRU.LRU(2);
+      lru.set('a', 'a');
+      lru.set('b', 'b');
+      var json1 = JSON.stringify(lru);
+
+      lru.get('b');
+      var json2 = JSON.stringify(lru);
+
+      assert.equal(json2, json1);
+    }
+  }
+});
+
+suite.export(module);
+
